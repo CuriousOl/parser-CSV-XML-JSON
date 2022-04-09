fc67ff2e8a691568a26bce2395b9889d1ae19871
Popova Olesya <popovaolesya87@gmail.com>
2022-04-09 21:47:41 +0300

    Add files

---
+++ src/main/java/ru/netology/Main.java
@@ -0,0 +1,153 @@
+package ru.netology;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.w3c.dom.*;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        //CSV -> JSON
+        String[] columnMapping = {"id", "firstName", "lastName", "country", "age"};
+        String fileName = "data.csv";
+        List<Employee> employees = parseCSV(columnMapping, fileName);
+
+        String jsonFromCSV = listToJson(employees);
+        String jsonFileName = "data.json";
+        writeString(jsonFileName, jsonFromCSV);
+
+        //XML -> JSON
+        List<Employee> list = null;
+        try {
+            list = parseXML("data.xml");
+        } catch (ParserConfigurationException | IOException | SAXException e) {
+            System.out.println(e.getMessage());
+        }
+        String jsonFromXML = listToJson(list);
+        writeString("data2.json", jsonFromXML);
+
+        //Parse JSON
+        String json = readString(jsonFileName);
+        List<Employee> listEmployees = jsonToList(json);
+        listEmployees.stream()
+                .forEach(System.out::println);
+    }
+
+    public static String readString(String fileName) {
+        String str = "";
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("data.json"));
+            JSONArray jsonArray = (JSONArray) obj;
+            str = jsonArray.toJSONString();
+        } catch (IOException | ParseException e) {
+            System.out.println(e.getMessage());
+        }
+        return str;
+    }
+
+    public static List<Employee> parseXML(String fileName) throws ParserConfigurationException, IOException, SAXException {
+        List<Employee> employees = new ArrayList<>();
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        Document doc = builder.parse(new File(fileName));
+        Node root = doc.getDocumentElement();
+        NodeList nodeList = doc.getElementsByTagName("employee");
+        for (int i = 0; i < nodeList.getLength(); i++) {
+            employees.add(getEmployee(nodeList.item(i)));
+        }
+        return employees;
+    }
+
+    public static Employee getEmployee(Node node) {
+        Employee employee = new Employee();
+        if (node.getNodeType() == Node.ELEMENT_NODE) {
+            Element element = (Element) node;
+            employee.setId(Integer.parseInt(getTagValue("id", element)));
+            employee.setFirstName(getTagValue("firstName", element));
+            employee.setLastName(getTagValue("lastName", element));
+            employee.setCountry(getTagValue("country", element));
+            employee.setAge(Integer.parseInt(getTagValue("age", element)));
+        }
+        return employee;
+    }
+
+    public static String getTagValue(String tag, Element element) {
+        NodeList nodeList = element.getElementsByTagName(tag).item(0).getChildNodes();
+        Node node = (Node) nodeList.item(0);
+        return node.getNodeValue();
+    }
+
+    public static List<Employee> jsonToList(String json) {
+        List<Employee> employees = new ArrayList<>();
+        JSONParser parser = new JSONParser();
+        try {
+            Object obj = parser.parse(new FileReader("data.json"));
+            JSONArray jsonArray = (JSONArray) obj;
+            for (int i = 0; i < jsonArray.size(); ++i) {
+                GsonBuilder builder = new GsonBuilder();
+                Gson gson = builder.create();
+                Employee employee = gson.fromJson(jsonArray.get(i).toString(), Employee.class);
+                employees.add(employee);
+            }
+        } catch (IOException | ParseException e) {
+            System.out.println(e.getMessage());
+        }
+        return employees;
+    }
+
+    public static void writeString(String jsonFileName, String json) {
+        try (FileWriter file = new FileWriter(jsonFileName)) {
+            file.write(json);
+            file.flush();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static String listToJson(List<Employee> employees) {
+        Type listType = new TypeToken<List<Employee>>() {
+        }.getType();
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+        String json = gson.toJson(employees, listType);
+        return json;
+    }
+
+    public static List<Employee> parseCSV(String[] columnMapping, String fileName) {
+        try (CSVReader csvReader = new CSVReader(new FileReader(fileName))) {
+            ColumnPositionMappingStrategy<Employee> strategy = new ColumnPositionMappingStrategy<>();
+            strategy.setType(Employee.class);
+            strategy.setColumnMapping("id", "firstName", "lastName", "country", "age");
+            CsvToBean<Employee> csv = new CsvToBeanBuilder<Employee>(csvReader)
+                    .withMappingStrategy(strategy)
+                    .build();
+            List<Employee> employees = csv.parse();
+            return employees;
+        } catch (IOException exc) {
+            System.out.println(exc.getMessage());
+            return new ArrayList<>();
+        }
+    }
+
+
+}
